4f99027f73296dd8f17cca811780cae5b6e139e8
unknown <tappannagari@gmail.com>
2021-04-29 22:10:25 -0700

    Planet Scenes

---
+++ Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EarthScene(){
+    	Debug.Log("Hello World");
+    	SceneManager.LoadScene("EarthScene");
+    }
+
+    public void Galaxy(){
+		Debug.Log("Hello World");
+    	SceneManager.LoadScene("SampleScene");
+    }
+    // Debug.Log("Hello World");
+}
